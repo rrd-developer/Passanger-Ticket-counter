8e165c5da3ff1bd9fb3fe1286d30dbbe26000f75
DHINAKAR R <123221939+rrd-developer@users.noreply.github.com>
2023-02-21 19:13:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+//document.getElementById.innerText = 5
+let saveEl= document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+function increment(){
+    count = count + 1;
+    countEl.textContent = count
+    
+}
+increment();
+
+function save() {
+    let countStr = count + "-"
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0;
+
+
+}
+save();
+
+
+
+
